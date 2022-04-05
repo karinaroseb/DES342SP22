7f1e350f81ef0b1eab830aaabac22d6072a80e05
karinaroseb <91501487+karinaroseb@users.noreply.github.com>
2022-04-05 12:08:53 -0700

    jquery fun + hw2

---
+++ homeworks/hw2/script.js
@@ -0,0 +1,45 @@
+$('.one').click(
+    function () {
+        $('.slide').attr('src', 'images/comic1.png')
+    }
+)
+$('.two').click(
+    function () {
+        $('.slide').attr('src', 'images/comic2.png')
+    }
+)
+$('.three').click(
+    function () {
+        $('.slide').attr('src', 'images/comic3.png')
+    }
+)
+$('.four').click(
+    function () {
+        $('.slide').attr('src', 'images/comic4.png')
+    }
+)
+$('.five').click(
+    function () {
+        $('.slide').attr('src', 'images/comic5.png')
+    }
+)
+$('.six').click(
+    function () {
+        $('.slide').attr('src', 'images/comic6.png')
+    }
+)
+$('.seven').click(
+    function () {
+        $('.slide').attr('src', 'images/comic7.png')
+    }
+)
+$('.eight').click(
+    function () {
+        $('.slide').attr('src', 'images/comic8.png')
+    }
+)
+$('.nine').click(
+    function () {
+        $('.slide').attr('src', 'images/comic9.png')
+    }
+)
---
+++ jquery-play/script.js
@@ -0,0 +1,35 @@
+// Im some javascript!
+//$('body').css('background', 'pink')
+$('h1').click(
+    function () {
+        $('body').toggleClass('purple')
+    }
+)
+// let rainbow = 0
+
+
+// let colorVal = `hsl(${rainbow}, 100, 100);`
+
+// $('button').click(
+//     function(){
+//         rainbow + 30
+//         $('body').css('background', `${colorVal}`)
+//     }
+// )
+
+
+$('.one').click(
+    function () {
+        $('.kitties').attr('src', 'http://placekitten.com/400/401')
+    }
+)
+$('.two').click(
+    function () {
+        $('.kitties').attr('src', 'http://placekitten.com/401/401')
+    }
+)
+$('.three').click(
+    function () {
+        $('.kitties').attr('src', 'http://placekitten.com/401/400')
+    }
+)
