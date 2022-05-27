b2704316d53e353ddef5e1dc779a183173773c34
karinaroseb <91501487+karinaroseb@users.noreply.github.com>
2022-05-26 17:10:58 -0700

    attempt at page animation

--- project1/script.js
+++ project1/script.js
@@ -1,31 +1,47 @@
 var audio;
 $(document).ready
 audio = $(".audioDemo");
-    addEventHandlers();
+addEventHandlers();
 
-function addEventHandlers(){
+function addEventHandlers() {
     $("a.load").click(loadAudio);
     $("a.start").click(startAudio);
     $("a.pause").click(pauseAudio);
     $("a.stop").click(stopAudio);
 }
 
-function loadAudio(){
-    audio.bind("load",function(){
+function loadAudio() {
+    audio.bind("load", function () {
         $(".alert-success").html("Audio Loaded succesfully");
     });
     audio.trigger('load');
 }
 
-function startAudio(){
+function startAudio() {
     audio.trigger('play');
 }
 
-function pauseAudio(){
+function pauseAudio() {
     audio.trigger('pause');
 }
 
-function stopAudio(){
+function stopAudio() {
     pauseAudio();
-    audio.prop("currentTime",0);
+    audio.prop("currentTime", 0);
 }
+
+//page close animation
+
+var tl = new TimelineMax({
+    repeat: 0,
+    yoyo: false
+})
+
+tl.to(".animated-rectangle", 1.5, {
+    scaleY: 0,
+    transformOrigin: "100% 0%",
+    duration: 7
+});
+
+tl.play();
+// {onComplete: window.location.replace}window.location.replace("index.html#timeline");
